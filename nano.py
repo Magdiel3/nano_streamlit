dc705a8dd3d17745d054da0df9090528c37246cd
Magdiel Martinez <magdieltercero@hotmail.com>
2025-06-11 23:13:23 -0600

    Loaded the dataset

--- nano.py
+++ nano.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+from os.path import basename
 
 # First, do some unessential modifications to the page
 st.set_page_config(
@@ -13,10 +15,31 @@ st.write("""
     This is a simple demo of a data exploration app using Streamlit.
 
     ## Objectives
-    - [ ] Load a dataset
+    - [X] Load a dataset
     - [ ] Display raw data
     - [ ] Do some basic data exploration
     - [ ] Add some basic pre-processing or cleanup
     - [ ] Add some basic interactive visualizations
     - [ ] Outline some non-scientific conclusions
 """)
+
+# DATASET LOADING
+
+dataset_name = "./datasets/CoffeeAndCodeLT2018---CoffeeAndCodeLT2018.csv"
+
+st.write(f"""
+         ### Dataset loading
+
+         This section may be not very useful from the user perspective, but it is an essential step.
+         The dataset `{basename(dataset_name)}` is loaded with the following code:
+         ```python
+         import pandas as pd
+
+         dataset_name = "{dataset_name}"
+         df = pd.read_csv(dataset_name, encoding="utf-8")
+         ```
+         """)
+
+dataset_name = "./datasets/CoffeeAndCodeLT2018---CoffeeAndCodeLT2018.csv"
+df = pd.read_csv(dataset_name, encoding="utf-8")
+
